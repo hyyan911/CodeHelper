064a257c824a861fe91a0abd83a002772eff3a78
hy911814 <hy911814@outlook.com>
2025-02-09 21:40:23 +0800

    2025.2.9

--- ColorHelper.cs
+++ ColorHelper.cs
@@ -21,7 +21,8 @@ namespace CodeHelper
             byte R = (byte)random.Next(256);
             byte G = (byte)random.Next(256);
             byte B = (byte)random.Next(256);
-            while (Math.Pow(CenterColor.R - R, 2) + Math.Pow(CenterColor.G - G, 2) + Math.Pow(CenterColor.B - B, 2) < 50)
+            double lum = GetContrastColor(CenterColor.R, CenterColor.G, CenterColor.B);
+            while (Math.Abs(lum - GetContrastColor(R, G, B)) < 50)
             {
                 R = (byte)random.Next(256);
                 G = (byte)random.Next(256);
@@ -29,5 +30,18 @@ namespace CodeHelper
             }
             return Color.FromRgb(R, G, B);
         }
+
+        /// <summary>
+        /// 根据亮度选择对比色（黑色或白色）
+        /// </summary>
+        /// <param name="color">原始颜色</param>
+        /// <returns>对比色</returns>
+        private static double GetContrastColor(byte R, byte G, byte B)
+        {
+            // 计算亮度
+            return 0.299 * R + 0.587 * G + 0.114 * B;
+
+        }
     }
 }
+
