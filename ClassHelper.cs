2f70d327c2c6197d0a3789c179581e7a9e8ac1fe
hy911814 <hy911814@outlook.com>
2025-02-18 19:54:49 +0800

    2025.2.18

---
+++ ClassHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeHelper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ClassHelper
+    {
+        /// <summary>
+        /// C#获取一个类在其所在的程序集中的所有子类
+        /// </summary>
+        /// <param name="parentType">给定的类型</param>
+        /// <returns>所有子类的名称</returns>
+        public static List<Type> GetSubClassTypes(Type parentType)
+        {
+            var subTypeList = new List<Type>();
+            var assembly = parentType.Assembly;//获取当前父类所在的程序集``
+            var assemblyAllTypes = assembly.GetTypes();//获取该程序集中的所有类型
+            foreach (var itemType in assemblyAllTypes)//遍历所有类型进行查找
+            {
+                var baseType = itemType.BaseType;//获取元素类型的基类
+                if (baseType != null)//如果有基类
+                {
+                    if (baseType.Name == parentType.Name)//如果基类就是给定的父类
+                    {
+                        subTypeList.Add(itemType);//加入子类表中
+                    }
+                }
+            }
+            return subTypeList;//获取所有子类类型的名称
+        }
+    }
+}
